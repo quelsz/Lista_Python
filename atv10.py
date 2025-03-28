93a0eaf5f9871ea5fd92074c1031050cce96c8d6
quelsz <124782622+quelsz@users.noreply.github.com>
2025-03-28 15:28:42 -0300

    Update atv10.py

--- atv10.py
+++ atv10.py
@@ -1,10 +1,8 @@
-#Solicite um número inteiro maior que 1 e verifique se ele é primo.
-
-n = int(input("Diga um numero inteiro maior que 1: "))
+num = int(input("Diga um numero inteiro maior que 1: "))
 
 primo = True
 for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+        if num % i == 0:
             primo = False
             break
           
